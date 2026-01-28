46aeb2035bb193f5b2016eb01344c52144a3f905
PeKkA01 <agulati395@gmail.com>
2026-01-29 01:59:39 +0530

    UC2 done

---
+++ src/com/banner/app/OopsBannerApp.java
@@ -0,0 +1,19 @@
+package com.banner.app;
+
+/**
+ * UC2: Render OOPS as Banner using Print Statements
+ * Displays the word "OOPS" using '*' and spaces.
+ */
+public class OopsBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****     *****     *****      ***** ");
+        System.out.println(" *     *   *     *   *     *    *       ");
+        System.out.println(" *     *   *     *   *     *     *****  ");
+        System.out.println(" *     *   *     *   *****            *");
+        System.out.println(" *     *   *     *   *                *");
+        System.out.println(" *     *   *     *   *                *");
+        System.out.println("  *****     *****    *          *****  ");
+    }
+}
